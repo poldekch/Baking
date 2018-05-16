25aebd0608bc5d426984a8995668dc426b547172
Leopold <leopold.chmielewski@gmail.com>
2018-05-16 20:03:23 +0100

    Loading recipes data

--- app/src/main/java/com/example/leopo/baking/MainActivity.java
+++ app/src/main/java/com/example/leopo/baking/MainActivity.java
@@ -6,6 +6,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
 import com.example.leopo.baking.utilities.NetworkUtils;
 
 import java.net.URL;
@@ -19,10 +24,25 @@ public class MainActivity extends AppCompatActivity {
     public static final String RECIPES_STATE_KEY = "recipes";
     private Parcelable mRecipesState;
 
+    private TextView mErrorMessageDisplay;
+    private ProgressBar mLoadingIndicator;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRecyclerView = findViewById(R.id.rv_recipes);
+        mErrorMessageDisplay = findViewById(R.id.tv_error_message_diaplay);
+        mLayoutManager = new GridLayoutManager(this, numberOfColumns());
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.setHasFixedSize(true);
+
+        // TODO add adapter
+
+        mLoadingIndicator = findViewById(R.id.pb_loading_indicator);
+
+        loadRecipeData();
     }
 
     @Override
@@ -55,8 +75,7 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
-            // TODO
-            // mLoadingIndicator.setVisibility(View.VISIBLE)
+            mLoadingIndicator.setVisibility(View.VISIBLE);
         }
 
         @Override
@@ -75,15 +94,40 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected void onPostExecute(ArrayList<Recipe> recipes) {
-            // TODO
-            // mLoadingIndicator.setVisibility(View.INVISIBLE);
+            mLoadingIndicator.setVisibility(View.INVISIBLE);
 
             if (recipes != null) {
                 // TODO implement
             } else {
                 // TODO
-//                showErrorMessage();
+                showErrorMessage();
             }
         }
+    }
+
+    private void loadRecipeData() {
+        showRecipeDataView();
+        new FetchRecipesTask().execute();
+    }
+
+    private void showRecipeDataView() {
+        mErrorMessageDisplay.setVisibility(View.INVISIBLE);
+        mRecyclerView.setVisibility(View.VISIBLE);
+    }
+
+    private void showErrorMessage() {
+        mRecyclerView.setVisibility(View.INVISIBLE);
+        mErrorMessageDisplay.setVisibility(View.VISIBLE);
+    }
+
+    private int numberOfColumns() {
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        // You can change this divider to adjust the size of the poster
+        int widthDivider = 400;
+        int width = displayMetrics.widthPixels;
+        int nColumns = width / widthDivider;
+        if (nColumns < 2) return 2; //to keep the grid aspect
+        return nColumns;
     }
 }
