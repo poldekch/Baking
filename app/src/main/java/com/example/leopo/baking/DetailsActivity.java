6c63ea1a3c6f3eb10e632b7b56b9c624cb09370e
Leopold <leopold@motorcheck.ie>
2018-05-21 12:18:59 +0100

    Details fragment

--- app/src/main/java/com/example/leopo/baking/DetailsActivity.java
+++ app/src/main/java/com/example/leopo/baking/DetailsActivity.java
@@ -1,13 +1,47 @@
 package com.example.leopo.baking;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.leopo.baking.data.Step;
+import com.example.leopo.baking.ui.DetailsFragment;
+
+import java.util.ArrayList;
+
 public class DetailsActivity extends AppCompatActivity {
+
+    private ArrayList<Step> mSteps;
+    private int stepPos;
+
+    private Fragment mFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        mFragment = fragmentManager.findFragmentByTag("DetailsFragment");
+
+        Bundle bundle = getIntent().getExtras();
+        // TODO key as constant
+        mSteps = bundle.getParcelableArrayList("steps");
+        stepPos = bundle.getInt("stepPos");
+
+        if (savedInstanceState == null) {
+            stepPos = bundle.getInt("stepPos");
+        } else {
+            // TODO key as const
+            stepPos = savedInstanceState.getInt("stepPos");
+        }
+
+        if (mFragment == null) {
+            mFragment = DetailsFragment.newInstance(mSteps, stepPos);
+            fragmentManager.beginTransaction()
+                    .replace(R.id.details_fragment_container, mFragment, "DetailsFragment")
+                    .commit();
+        }
     }
 }
---
+++ app/src/main/java/com/example/leopo/baking/ui/DetailsFragment.java
@@ -0,0 +1,29 @@
+package com.example.leopo.baking.ui;
+
+import android.app.Fragment;
+import android.os.Bundle;
+
+import com.example.leopo.baking.data.Step;
+
+import java.util.ArrayList;
+
+public class DetailsFragment extends Fragment{
+
+    public DetailsFragment() {}
+
+
+    // TODO review
+    public static DetailsFragment newInstance(ArrayList<Step> steps, int position) {
+
+        DetailsFragment detailsFragment = new DetailsFragment();
+        Bundle bundle = new Bundle();
+        bundle.putParcelableArrayList("Steps", steps);
+        bundle.putInt("Position", position);
+
+        detailsFragment.setArguments(bundle);
+
+        return detailsFragment;
+    }
+
+
+}
