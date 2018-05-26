ca59ef9a0f8e9d6cc5d1aab6a8c8928ff34dbf3a
Leopold <leopold.chmielewski@gmail.com>
2018-05-26 19:48:00 +0100

    Player

--- app/src/main/java/com/example/leopo/baking/ui/RecipeDetailsFragment.java
+++ app/src/main/java/com/example/leopo/baking/ui/RecipeDetailsFragment.java
@@ -1,9 +1,16 @@
 package com.example.leopo.baking.ui;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.media.session.MediaButtonReceiver;
+import android.support.v4.media.session.MediaSessionCompat;
 import android.os.Bundle;
+import android.support.v4.media.session.PlaybackStateCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,16 +19,32 @@ import android.widget.TextView;
 
 import com.example.leopo.baking.R;
 import com.example.leopo.baking.data.Step;
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.ExoPlaybackException;
+import com.google.android.exoplayer2.ExoPlayer;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.LoadControl;
+import com.google.android.exoplayer2.PlaybackParameters;
 import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.Timeline;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.TrackGroupArray;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelectionArray;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
 import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
 import com.squareup.picasso.Picasso;
-
-import java.util.ArrayList;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class RecipeDetailsFragment extends Fragment{
+public class RecipeDetailsFragment extends Fragment implements ExoPlayer.EventListener {
+
+    private static final String TAG = RecipeDetailsFragment.class.getSimpleName();
 
     Step mStep;
     @BindView(R.id.ep_video)
@@ -29,7 +52,11 @@ public class RecipeDetailsFragment extends Fragment{
     @BindView(R.id.iv_missing_video)
     ImageView missingVideo;
     @BindView(R.id.tv_step_description) TextView stepDescription;
-    SimpleExoPlayer exoPlayer;
+    SimpleExoPlayer mExoPlayer;
+
+    private Uri mVideoUri;
+    private PlaybackStateCompat.Builder playbackBuilder;
+    private static MediaSessionCompat mMediaSession;
 
     public RecipeDetailsFragment() {}
 
@@ -63,13 +90,119 @@ public class RecipeDetailsFragment extends Fragment{
                     Picasso.with(getContext()).load(mStep.getThumbnailURL()).into(missingVideo);
                 }
 
+            } else {
+                if (savedInstanceState != null) {
+                    // todo retrieve
+                }
+                mVideoUri = Uri.parse(mStep.getVideoURL());
+                missingVideo.setVisibility(View.GONE);
+                initialiseMediaSession();
+                initialisePlayer(mVideoUri);
+
+
             }
         } else {
             exoPlayerView.setVisibility(View.GONE);
         }
 
-
-
         return view;
     }
+
+    private void initialiseMediaSession() {
+        mMediaSession = new MediaSessionCompat(getActivity(), TAG);
+        mMediaSession.setFlags(MediaSessionCompat.FLAG_HANDLES_MEDIA_BUTTONS | MediaSessionCompat.FLAG_HANDLES_TRANSPORT_CONTROLS);
+        mMediaSession.setMediaButtonReceiver(null);
+        playbackBuilder = new PlaybackStateCompat.Builder()
+                .setActions(
+                        PlaybackStateCompat.ACTION_PLAY |
+                        PlaybackStateCompat.ACTION_PAUSE |
+                        PlaybackStateCompat.ACTION_SKIP_TO_PREVIOUS |
+                        PlaybackStateCompat.ACTION_PLAY_PAUSE
+                );
+        mMediaSession.setPlaybackState(playbackBuilder.build());
+        mMediaSession.setCallback(new RecipeDetailsCallbacks());
+        mMediaSession.setActive(true);
+    }
+
+    private void initialisePlayer(Uri videoUri) {
+        if (mExoPlayer == null) {
+            TrackSelector trackSelector = new DefaultTrackSelector();
+            LoadControl loadControl = new DefaultLoadControl();
+            mExoPlayer = ExoPlayerFactory.newSimpleInstance(getActivity(), trackSelector, loadControl);
+            mExoPlayer.addListener(this);
+            String userAgent = Util.getUserAgent(getActivity(), getActivity().getString(R.string.exo_player));
+            MediaSource mediaSource = new ExtractorMediaSource(videoUri, new DefaultDataSourceFactory(getActivity(), userAgent), new DefaultExtractorsFactory(), null, null);
+            mExoPlayer.prepare(mediaSource);
+            mExoPlayer.setPlayWhenReady(true);
+        }
+    }
+
+//    @Override
+//    public void onResume() {
+//        super.onResume();
+//        if (mExoPlayer != null) {
+//            mExoPlayer.setPlayWhenReady(playWhenReady);
+//            mExoPlayer.seekTo(positionPlayer);
+//        } else {
+//            initialiseMediaSession();
+//            initialisePlayer(mVideoUri);
+//            mExoPlayer.setPlayWhenReady(playWhenReady);
+//
+//        }
+//    }
+
+    @Override
+    public void onTimelineChanged(Timeline timeline, Object manifest) {}
+
+    @Override
+    public void onTracksChanged(TrackGroupArray trackGroups, TrackSelectionArray trackSelections) {}
+
+    @Override
+    public void onLoadingChanged(boolean isLoading) {}
+
+    @Override
+    public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
+        if (playbackState == ExoPlayer.STATE_READY && playWhenReady) {
+            playbackBuilder.setState(PlaybackStateCompat.STATE_PLAYING, mExoPlayer.getCurrentPosition(), 1f);
+        } else if (playbackState == ExoPlayer.STATE_READY) {
+            playbackBuilder.setState(PlaybackStateCompat.STATE_PAUSED, mExoPlayer.getCurrentPosition(), 1f);
+        }
+        mMediaSession.setPlaybackState(playbackBuilder.build());
+    }
+
+    @Override
+    public void onPlayerError(ExoPlaybackException error) {}
+
+    @Override
+    public void onPositionDiscontinuity() {}
+
+    @Override
+    public void onPlaybackParametersChanged(PlaybackParameters playbackParameters) {}
+
+    private class RecipeDetailsCallbacks extends MediaSessionCompat.Callback {
+        @Override
+        public void onPlay() {
+            mExoPlayer.setPlayWhenReady(true);
+        }
+
+        @Override
+        public void onPause() {
+            mExoPlayer.setPlayWhenReady(false);
+        }
+
+        @Override
+        public void onSkipToPrevious() {
+            mExoPlayer.seekTo(0);
+        }
+    }
+
+    public static class MediaReceiver extends BroadcastReceiver {
+        public MediaReceiver() {}
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            MediaButtonReceiver.handleIntent(mMediaSession, intent);
+        }
+    }
+
 }
