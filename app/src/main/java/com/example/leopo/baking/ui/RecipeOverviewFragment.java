2824334df4169fe4af521ddb8747abf9808f076a
Leopold <leopold.chmielewski@gmail.com>
2018-05-27 16:51:27 +0100

    Removed lots of comments

--- app/src/main/java/com/example/leopo/baking/ui/RecipeOverviewFragment.java
+++ app/src/main/java/com/example/leopo/baking/ui/RecipeOverviewFragment.java
@@ -35,10 +35,13 @@ public class RecipeOverviewFragment extends Fragment implements ClickCallBack {
 
     // TODO save instance
 
-    @BindView(R.id.tv_ingredients) TextView ingredients;
-    @BindView(R.id.rv_steps) RecyclerView recyclerView;
+    @BindView(R.id.tv_ingredients)
+    TextView ingredients;
+    @BindView(R.id.rv_steps)
+    RecyclerView recyclerView;
 
-    public RecipeOverviewFragment() {}
+    public RecipeOverviewFragment() {
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -56,7 +59,7 @@ public class RecipeOverviewFragment extends Fragment implements ClickCallBack {
         unbinder = ButterKnife.bind(this, rootView);
 
         String ingredientsString = new String();
-        for (int i=0; i<=mIngredients.size()-1; i++) {
+        for (int i = 0; i <= mIngredients.size() - 1; i++) {
             Ingredient ingredient = mIngredients.get(i);
             ingredientsString = ingredientsString + "- " + ingredient.getQuantity() + " " + ingredient.getMeasure() + " of " + ingredient.getIngredient() + "\n";
         }
@@ -96,92 +99,4 @@ public class RecipeOverviewFragment extends Fragment implements ClickCallBack {
             context.startActivity(intent);
         }
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//    @Override
-//    public void onStepSelected(int position) {
-//        Toast.makeText(getContext(), "Position " + position, Toast.LENGTH_LONG).show();
-//    }
-
-    // TODO mCallback
-//    OnStepClickListener mCallback;
-
-//    public interface OnStepClickListener {
-//        void onStepSelected(int position);
-//    }
-
-//    @Override
-//    public void onAttach(Context context) {
-//        super.onAttach(context);
-//
-//        try {
-//            mCallback = (OnStepClickListener) context;
-//        } catch (ClassCastException e) {
-//            throw new ClassCastException(context.toString() + " must implement OnStepClickListener");
-//        }
-//    }
-
-//    public interface StepCallback{
-//        void onStepSelected(ArrayList<Step> steps, int position);
-//    }
-
-//    public RecipeStepsFragment() {}
-
-
-    public void setStepsData(ArrayList<Step> steps) {
-        mSteps = steps;
-    }
-
-
-//
-//    // TODO save instance
-//    public static String INGREDIENTS = "ingredients";
-//
-//    @BindView(R.id.rv_ingredients)RecyclerView recyclerView;
-//
-//    private ArrayList<Ingredient> mIngredients;
-//    private Unbinder unbinder;
-
-    // TODO mCallback
-
-
-//    public RecipeIngredientsFragment() {}
-
-//    @Override
-//    public void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-////        mRecipe = getArguments().getParcelable(RECIPE);
-//    }
-
-//    @Override
-//    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-//
-//        View rootView = inflater.inflate(R.layout.fragment_recipe_overview, container, false);
-//        unbinder = ButterKnife.bind(this, rootView);
-//
-//        IngredientAdapter mAdapter = new IngredientAdapter(getContext(), mIngredients);
-//        recyclerView.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false));
-//        recyclerView.setAdapter(mAdapter);
-//
-//        return rootView;
-//    }
-//
-//    public void setIngredientsData(ArrayList<Ingredient> ingredients) {
-//        mIngredients = ingredients;
-//    }
-
-
 }
